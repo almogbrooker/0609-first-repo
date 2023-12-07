5c5841375e593539afb4ad6b744a6b2f2e6edd89
almog brooker <almogg1000@gmail.com>
2023-12-07 19:31:03 +0200

    new feature  (#1)
    * first commit
    
    * second changes

--- class_7.py
+++ class_7.py
@@ -3,4 +3,8 @@ from os import getenv
 env = getenv("ENVITOMENT")
 action = getenv("ACTION")
 if env == "dev" and action == "stop":
-        print("stop")
+        print("good")
+
+
+if 2 == 2:
+        print("2")
